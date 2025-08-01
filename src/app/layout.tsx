529424b2c3506e541d9a9408703e113d54e9e49e
Parv <parvshah010806@gmail.com>
2025-08-01 18:08:11 +0530

    V1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/header';
 import Footer from '@/components/Footer';
 import ClientWrapper from '@/components/ClientWrapper';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Parv’s Portfolio',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
+        <SpeedInsights/>
         <Analytics/>
         <ClientWrapper>
           <Header />
